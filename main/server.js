64b57988fd3085f8cf58d49deb51c713b326046a
Simon Garnotel <simon.garnotel@gmail.com>
2020-07-28 16:57:07 +0200

    try express

---
+++ main/server.js
@@ -0,0 +1,10 @@
+// import express from 'express'
+// const app = express()
+
+// import login from '../renderer/pages/api/user/login'
+
+// app.post('/api/user/login', (req, res) => {
+//   return login(req, res)
+// })
+
+// app.listen(3000)

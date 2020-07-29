89dd9484e3b2d6fab4cb9bb97533a7dce5765192
Simon Garnotel <simon.garnotel@gmail.com>
2020-07-29 15:53:28 +0200

    Nextron (#11)
    * dev + build ok
    
    * cleanup
    
    * missing dep & cleanup

---
+++ main/background.js
@@ -0,0 +1,48 @@
+import { app, ipcMain } from 'electron'
+import serve from 'electron-serve'
+import { createWindow } from './helpers'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+if (isProd) {
+  serve({ directory: 'app' })
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`)
+}
+
+;(async () => {
+  console.info('Starting Tanatloc')
+  await app.whenReady()
+
+  // Server
+  console.info('Starting server')
+  require('../dist-server/server/bin/www')
+
+  // Client
+  console.info('Starting client')
+  const mainWindow = createWindow('main', {
+    width: 1000,
+    height: 600
+  })
+  mainWindow.maximize()
+
+  if (isProd) {
+    await mainWindow.loadURL('app://./index.html')
+  } else {
+    const port = process.argv[2]
+    await mainWindow.loadURL(`http://localhost:${port}/`)
+    mainWindow.webContents.openDevTools()
+  }
+})()
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on('ping-pong', (event, arg) => {
+  event.sender.send('ping-pong', `[ipcMain] "${arg}" received asynchronously.`)
+})
+
+ipcMain.on('ping-pong-sync', (event, arg) => {
+  event.returnValue = `[ipcMain] "${arg}" received synchronously.`
+})

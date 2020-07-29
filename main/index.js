f5904b42e27cd8415e27929dbde40793ee1f4306
Simon Garnotel <simon.garnotel@gmail.com>
2020-07-29 12:33:14 +0200

    electron-build try

--- main/index.js
+++ main/index.js
@@ -9,10 +9,8 @@ const prepareNext = require('electron-next')
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
-  // // Server
-  // isDev
-  //   ? require(join(__dirname, '../dist-server/server/bin/www'))
-  //   : require(join(__dirname, '../../dist-server/server/bin/www'))
+  // Server
+  require(join(__dirname, '../dist-server/server/bin/www'))
 
   // Client
   await prepareNext('./renderer')

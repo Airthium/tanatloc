a7f0d1f000125915268486ffb6691ad8c8e8ec22
Simon Garnotel <simon.garnotel@gmail.com>
2020-11-12 16:18:19 +0100

    tests

--- renderer/__tests__/components/signup/index.test.js
+++ renderer/__tests__/components/signup/index.test.js
@@ -1,6 +1,17 @@
 import Signup from '../../../components/signup'
 import { shallow, mount } from 'enzyme'
 
+import '../../../../config/jest/matchMediaMock'
+
+const mockPrefetch = jest.fn()
+const mockPush = jest.fn()
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    prefetch: mockPrefetch,
+    push: mockPush
+  })
+}))
+
 jest.mock('../../../components/loading', () => 'loading')
 
 const mockUser = jest.fn()
@@ -12,6 +23,8 @@ jest.mock('../../../../src/api/user', () => ({
 let wrapper
 describe('renderer/components/signup', () => {
   beforeEach(() => {
+    mockPrefetch.mockReset()
+    mockPush.mockReset()
     mockUser.mockReset()
     mockLoading.mockReset()
     wrapper = shallow(<Signup />)
@@ -23,5 +36,25 @@ describe('renderer/components/signup', () => {
 
   it('render', () => {
     expect(wrapper).toBeDefined()
+  })
+
+  it('with user', () => {
+    wrapper.unmount()
+    mockUser.mockImplementation(() => ({}))
+    mockLoading.mockImplementation(() => false)
+    wrapper = shallow(<Signup />)
+  })
+
+  it('effect', () => {
+    wrapper.unmount()
+    wrapper = mount(<Signup />)
+    expect(mockPrefetch).toHaveBeenCalledTimes(1)
+    expect(mockPush).toHaveBeenCalledTimes(0)
+
+    wrapper.unmount()
+    mockUser.mockImplementation(() => ({}))
+    wrapper = mount(<Signup />)
+    expect(mockPrefetch).toHaveBeenCalledTimes(2)
+    expect(mockPush).toHaveBeenCalledTimes(1)
   })
 })

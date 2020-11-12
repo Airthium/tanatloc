868a8ee5c2f9b0b750bf296b9d5ad8c04279d10d
franckl <franck.lahaye@gmail.com>
2020-11-12 14:14:24 +0100

    updated login test, new login alert component and css update

--- renderer/components/login/index.js
+++ renderer/components/login/index.js
@@ -10,13 +10,13 @@ import {
   Form,
   Input,
   Layout,
-  Menu,
-  Space
+  Space,
+  Typography,
+  Alert
 } from 'antd'
-import { HomeOutlined } from '@ant-design/icons'
 
 import Loading from '../loading'
-import Background from '../background'
+//import Background from '../background'
 
 import login from '../../../src/api/login'
 import { useUser } from '../../../src/api/user'
@@ -26,13 +26,6 @@ import { useUser } from '../../../src/api/user'
  */
 const errors = {
   BAD_CREDENTIALS: 'Login failed. Please check your username and password'
-}
-
-/**
- * Menu keys
- */
-const menuKeys = {
-  home: 'home'
 }
 
 /**
@@ -57,21 +50,6 @@ const Login = () => {
   useEffect(() => {
     router.prefetch('/dashboard')
   }, [])
-
-  /**
-   * On select
-   * @param {Object} data Data { item, key }
-   */
-  const onSelect = ({ item, key }) => {
-    if (key === menuKeys.home) handleHome()
-  }
-
-  /**
-   * Handle home
-   */
-  const handleHome = () => {
-    router.push('/')
-  }
 
   /**
    * Handle login
@@ -102,54 +80,66 @@ const Login = () => {
       {loadingUser || user ? (
         <Loading />
       ) : (
-        <Layout className="tanatloc-gradient">
-          <Background />
-          <Layout.Header className="Login-header">
-            <Menu onSelect={onSelect} selectedkeys={[]}>
-              <Menu.Item key={menuKeys.home} icon={<HomeOutlined />}>
-                Home
-              </Menu.Item>
-            </Menu>
-          </Layout.Header>
-          <Card className="Login">
+        <Layout className="">
+          {/*<Background />*/}
+          <Card bordered={false} className="Login">
             <Space direction="vertical" size="large" style={{ width: '100%' }}>
-              <div className="logo">
-                <img src="/images/logo.svg" />
+              <div>
+                <Typography.Title
+                  level={1}
+                  style={{ padding: 0, marginBottom: 16, fontWeight: 500 }}
+                >
+                  Log In
+                </Typography.Title>
+                <Typography.Text>
+                  Your first time ? <a href="">Sign up</a>
+                </Typography.Text>
               </div>
-              <Form initialValues={{ remember: true }} onFinish={onLogin}>
+              <Form
+                initialValues={{ remember: true }}
+                onFinish={onLogin}
+                layout="vertical"
+              >
+                <Alert
+                  message="Incorrect credentials."
+                  type="error"
+                  showIcon
+                  style={{ marginBottom: '16px' }}
+                />
+                {/*
+                TODO We have 2 cases to handle
+              1) The user forgot to enter a value in the email or password field => validateStatus=error
+              2) The user enters the wrong credentials => the fields keep the default border color and we show an Alert component
+              */}
                 <Form.Item
                   name="username"
-                  rules={[
-                    { required: true, message: 'Please fill your Username!' }
-                  ]}
+                  label="Your email address"
+                  rules={[{ message: 'Please enter your email' }]}
+                  hasFeedback
+                  validateStatus="error"
                 >
-                  <Input placeholder="username" autoComplete="username" />
+                  <Input placeholder="Email address" autoComplete="email" />
                 </Form.Item>
                 <Form.Item
                   name="password"
-                  rules={[
-                    { required: true, message: 'Please fill your Password!' }
-                  ]}
+                  label="Your password"
+                  rules={[{ message: 'Please enter your Password' }]}
+                  style={{ marginBottom: '14px' }}
+                  hasFeedback
+                  validateStatus="error"
                 >
                   <Input.Password
-                    placeholder="password"
+                    placeholder="Password"
                     autoComplete="current-password"
                   />
                 </Form.Item>
-                <Form.Item>
-                  <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>remember me</Checkbox>
-                  </Form.Item>
-
-                  <a className="Login-forgot" href="">
-                    forgot password
-                  </a>
-                </Form.Item>
+                <Typography.Text>
+                  <a href="">Forgot your password ?</a>
+                </Typography.Text>
                 <Form.Item className="Login-submit">
                   <Button type="primary" loading={checking} htmlType="submit">
                     Log in
                   </Button>
-                  <a href="">or register now!</a>
                 </Form.Item>
               </Form>
             </Space>

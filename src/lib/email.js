3b89a9295b46fa763b0db6a6068dee5ada2b69b7
Simon Garnotel <simon.garnotel@gmail.com>
2021-02-11 10:30:03 +0100

    start email - wait for templates

---
+++ src/lib/email.js
@@ -0,0 +1,25 @@
+import MailerSend, { Recipient, EmailParams } from 'mailersend'
+
+import { TOKEN } from '@/config/email'
+
+const mailerSend = new MailerSend({
+  api_key: TOKEN
+})
+
+/**
+ * Subscribe
+ * @param {string} email Email
+ * @param {string} id User id
+ */
+const subscribe = (email, id) => {
+  const recipients = [new Recipient(email)]
+  const emailParams = new EmailParams()
+    .setFrom('subscribe@tanatloc.com')
+    .setFromName('Tanatloc')
+    .setRecipients(recipients)
+    .setSubject('Tanatloc subscription')
+    .setHtml('test')
+  mailerSend.send(emailParams)
+}
+
+export default { subscribe }

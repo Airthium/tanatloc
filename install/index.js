1d0e34579f2cc5d95a54e0235e8403d20a101dbc
Simon Garnotel <simon.garnotel@gmail.com>
2021-08-25 11:26:01 +0200

    banner

--- install/index.js
+++ install/index.js
@@ -8,6 +8,10 @@ import createPaths from './storage'
  * Main
  */
 const main = async () => {
+  console.info('/__   \\__ _ _ __   __ _| |_| | ___   ___ ')
+  console.info("  / /\\/ _` | '_ \\ / _` | __| |/ _ \\ / __|")
+  console.info(' / / | (_| | | | | (_| | |_| | (_) | (__ ')
+  console.info(' \\/   \\__,_|_| |_|\\__,_|\\__|_|\\___/ \\___|')
   await createDatabase()
   await update()
   await createPaths()

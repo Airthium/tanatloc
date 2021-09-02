687aceb635b103dc47c9a444d56caca58b20fdd7
Simon Garnotel <simon.garnotel@gmail.com>
2021-09-02 13:49:09 +0200

    v0.10.0

--- src/auth/password-local.js
+++ src/auth/password-local.js
@@ -8,8 +8,9 @@ export const localStrategy = new Local.Strategy(
   (email, password, done) => {
     UserDB.getByUsernameAndPassword({ email, password })
       .then((user) => {
-        if (!user.isvalidated)
-          throw new Error('User email is not validated yet!')
+        if (!user) done(new Error('Bad credentials!'))
+        else if (!user.isvalidated)
+          done(new Error('User email is not validated yet!'))
         done(null, user)
       })
       .catch((error) => {

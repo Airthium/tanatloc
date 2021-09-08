240b5988c3ceb68a09ecf4e222bf8baa07c987cf
Simon Garnotel <simon.garnotel@gmail.com>
2021-09-08 11:26:55 +0200

    v0.10.1

---
+++ .github/release.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then
+    echo "Provide a commit description"
+    exit 1
+fi
+
+branch=`git rev-parse --symbolic-full-name --abbrev-ref HEAD`
+
+if [ $branch = "dev" ]
+then
+    git add .
+    git commit -m"$1"
+    git push
+
+    git checkout hotfix
+    git merge dev
+    git push
+
+    git checkout master
+    git merge dev
+    git push
+
+    git checkout dev
+else
+    echo "You must be on dev branch to release"
+fi

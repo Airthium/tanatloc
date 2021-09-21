4d6e262673471fe39f99e3187a3c79357682aedf
Simon Garnotel <simon.garnotel@gmail.com>
2021-09-21 10:50:30 +0200

    module optim

--- modules/three-to-glb/index.js
+++ modules/three-to-glb/index.js
@@ -12,28 +12,4 @@ const threeToGlb = async (location, name) => {
   return convert(location, name)
 }
 
-// Geometry
-threeToGlb(
-  '/home/simon/tanatloc/geometry/rc-upload-1627976306928-4',
-  'part.json'
-)
-  .then(() => console.log('geometry'))
-  .catch(console.log)
-
-// Mesh
-threeToGlb(
-  '/home/simon/tanatloc/simulation/1ad4b16b-92b0-41f8-9277-54246118af7f/rc-upload-1630655266535-2_mesh/rc-upload-1630655266535-2',
-  'part.json'
-)
-  .then(() => console.log('mesh'))
-  .catch(console.log)
-
-// Result
-threeToGlb(
-  '/home/simon/tanatloc/simulation/1ad4b16b-92b0-41f8-9277-54246118af7f/run/result/Result_Presure',
-  'part.json'
-)
-  .then(() => console.log('mesh'))
-  .catch(console.log)
-
 module.exports = { convert: threeToGlb }

ea9abb25215870823fc1aeb67957bb7d9505b63e
Simon Garnotel <simon.garnotel@gmail.com>
2022-01-10 16:04:25 +0100

    index

--- src/components/indexpage/index.tsx
+++ src/components/indexpage/index.tsx
@@ -136,110 +136,120 @@ const Index = (): JSX.Element => {
           backgroundColor: 'white',
           overflow: 'auto'
         }}
+        className="Index-content"
       >
         <Typography.Title>See the world the way it really is!</Typography.Title>
 
         <Divider />
 
-        <Space wrap>
-          <Card
-            className="Index-card"
-            hoverable
-            cover={<img alt="Tanatloc" src="/images/Tanatloc.png" />}
-          >
-            <Card.Meta
-              title={
-                <Typography.Text>Easy simulation playground!</Typography.Text>
-              }
-              description={
-                <Typography.Text>
-                  Complete configuration using graphic interface
-                </Typography.Text>
-              }
-            />
-          </Card>
-          <Card
-            className="Index-card"
-            hoverable
-            cover={<TeamOutlined style={{ fontSize: '50px' }} />}
-          >
-            <Card.Meta
-              title={
-                <Typography.Text>User, group, organization!</Typography.Text>
-              }
-              description={
-                <Typography.Text>Manage your team easily</Typography.Text>
-              }
-            />
-          </Card>
-          <Card
-            className="Index-card"
-            hoverable
-            cover="$$\int_{\Omega}\nabla u\nabla v$$"
-          >
-            <Card.Meta
-              title={
-                <Typography.Text>Already available models!</Typography.Text>
-              }
-              description={
-                <Typography.Text>
-                  Fluid mechanics, solid mechanics, thermic, ...
-                </Typography.Text>
-              }
-            />
-          </Card>
-          <Card
-            className="Index-card coming-soon"
-            hoverable
-            cover="$$\int_{\Omega}\nabla u\nabla v$$"
-          >
-            <Card.Meta
-              title={<Typography.Text>Custom model editor!</Typography.Text>}
-              description={<Typography.Text>Coming soon</Typography.Text>}
-            />
-          </Card>
-          <Card
-            className="Index-card"
-            hoverable
-            cover={
-              <img
-                alt="FreeFEM"
-                src="/images/FreeFEM.svg"
-                style={{ paddingRight: '20px' }}
+        <div className="Index-cols">
+          <div className="Index-row">
+            <Card
+              className="Index-card"
+              hoverable
+              cover={<img alt="Tanatloc" src="/images/Tanatloc.png" />}
+            >
+              <Card.Meta
+                title={
+                  <Typography.Text>Easy simulation playground!</Typography.Text>
+                }
+                description={
+                  <Typography.Text>
+                    Complete configuration using graphic interface
+                  </Typography.Text>
+                }
               />
-            }
-          >
-            <Card.Meta
-              title={
-                <Typography.Text>
-                  Powered by{' '}
-                  <Button
-                    type="link"
-                    href="https://freefem.org/"
-                    target="_blank"
-                  >
-                    FreeFEM
-                  </Button>
-                  !
-                </Typography.Text>
+            </Card>
+            <Card
+              className="Index-card"
+              hoverable
+              cover={
+                <TeamOutlined style={{ fontSize: '50px', padding: '20px' }} />
               }
-              description="Finite element engine"
-            />
-          </Card>
-          <Card
-            className="Index-card"
-            hoverable
-            cover={
-              <Typography.Title style={{ textAlign: 'center' }}>
-                &lt;%= %&gt;
-              </Typography.Title>
-            }
-          >
-            <Card.Meta
-              title={<Typography.Text>Script template ready!</Typography.Text>}
-            />
-          </Card>
-        </Space>
+            >
+              <Card.Meta
+                title={
+                  <Typography.Text>User, group, organization!</Typography.Text>
+                }
+                description={
+                  <Typography.Text>Manage your team easily</Typography.Text>
+                }
+              />
+            </Card>
+
+            <Card
+              className="Index-card"
+              hoverable
+              cover="$$\int_{\Omega}\nabla u\nabla v$$"
+            >
+              <Card.Meta
+                title={
+                  <Typography.Text>Already available models!</Typography.Text>
+                }
+                description={
+                  <Typography.Text>
+                    Fluid mechanics, solid mechanics, thermic, ...
+                  </Typography.Text>
+                }
+              />
+            </Card>
+          </div>
+          <div className="Index-row">
+            <Card
+              className="Index-card coming-soon"
+              hoverable
+              cover="$$\int_{\Omega}\nabla u\nabla v$$"
+            >
+              <Card.Meta
+                title={<Typography.Text>Custom model editor!</Typography.Text>}
+                description={<Typography.Text>Coming soon</Typography.Text>}
+              />
+            </Card>
+            <Card
+              className="Index-card"
+              hoverable
+              cover={
+                <img
+                  alt="FreeFEM"
+                  src="/images/FreeFEM.svg"
+                  style={{ paddingRight: '20px' }}
+                />
+              }
+            >
+              <Card.Meta
+                title={
+                  <Typography.Text>
+                    Powered by{' '}
+                    <Button
+                      type="link"
+                      href="https://freefem.org/"
+                      target="_blank"
+                    >
+                      FreeFEM
+                    </Button>
+                    !
+                  </Typography.Text>
+                }
+                description="Finite element engine"
+              />
+            </Card>
+            <Card
+              className="Index-card"
+              hoverable
+              cover={
+                <Typography.Title style={{ textAlign: 'center' }}>
+                  &lt;%= %&gt;
+                </Typography.Title>
+              }
+            >
+              <Card.Meta
+                title={
+                  <Typography.Text>Script template ready!</Typography.Text>
+                }
+              />
+            </Card>
+          </div>
+        </div>
       </Layout.Content>
       <Layout.Footer style={{ background: 'none' }}>
         <Space

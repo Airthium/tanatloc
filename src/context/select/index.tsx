0411452bc727349e2505816ca68ba87f6723d23a
Simon Garnotel <simon.garnotel@gmail.com>
2022-03-11 14:00:31 +0100

    select context

---
+++ src/context/select/index.tsx
@@ -0,0 +1,216 @@
+import { createContext, ReactNode } from 'react'
+
+/**
+ * Select interface
+ */
+export interface ISelect {
+  uuid: string
+  label: number | string
+}
+
+/**
+ * Select state interface
+ */
+export interface ISelectState {
+  enabled: boolean
+  type?: string
+  part?: string
+  highlighted?: ISelect
+  selected: ISelect[]
+}
+
+/**
+ * Select action interface
+ */
+export interface ISelectAction {
+  type: string
+  value?: string | ISelect
+}
+
+/**
+ * Select initial state
+ */
+export const initialState: ISelectState = {
+  enabled: false,
+  type: null,
+  part: null,
+  highlighted: null,
+  selected: []
+}
+
+/**
+ * Action types
+ */
+export const actionTypes = {
+  ENABLE: 'ENABLE',
+  DISABLE: 'DISABLE',
+  CLEAR: 'CLEAR',
+  SETTYPE: 'SETTYPE',
+  SETPART: 'SETPART',
+  HIGHLIGHT: 'HIGHLIGHT',
+  UNHIGHLIGHT: 'UNHIGHLIGHT',
+  SELECT: 'SELECT',
+  UNSELECT: 'UNSELECT'
+}
+
+/**
+ * Context
+ */
+export const SelectContext = createContext(initialState)
+
+/**
+ * Reducer
+ * @param state State
+ * @param action Action
+ * @returns State
+ */
+export const selectReducer = (
+  state: ISelectState,
+  action: ISelectAction
+): ISelectState => {
+  let index: number
+
+  switch (action.type) {
+    case actionTypes.ENABLE:
+      return { ...state, enabled: true }
+    case actionTypes.DISABLE:
+      return { ...state, enabled: false }
+    case actionTypes.CLEAR:
+      return { ...initialState }
+    case actionTypes.SETTYPE:
+      return { ...state, type: action.value as string }
+    case actionTypes.SETPART:
+      return { ...state, part: action.value as string }
+    case actionTypes.HIGHLIGHT:
+      return { ...state, highlighted: action.value as ISelect }
+    case actionTypes.UNHIGHLIGHT:
+      return { ...state, highlighted: null }
+    case actionTypes.SELECT:
+      index = state.selected.findIndex(
+        (item: ISelect) => item.uuid === (action.value as ISelect).uuid
+      )
+      if (index === -1)
+        return {
+          ...state,
+          selected: [...state.selected, action.value as ISelect]
+        }
+      else return state
+    case actionTypes.UNSELECT:
+      index = state.selected.findIndex(
+        (item: ISelect) => item.uuid === (action.value as ISelect).uuid
+      )
+      if (index === -1) return state
+      else
+        return {
+          ...state,
+          selected: [
+            ...state.selected.slice(0, index),
+            ...state.selected.slice(index + 1)
+          ]
+        }
+    default:
+      return state
+  }
+}
+
+/**
+ * Enable
+ */
+export const enable = () => {
+  selectReducer(initialState, { type: actionTypes.ENABLE })
+}
+
+/**
+ * Disable
+ */
+export const disable = () => {
+  selectReducer(initialState, { type: actionTypes.DISABLE })
+}
+
+/**
+ * Clear
+ */
+export const clear = () => {
+  selectReducer(initialState, { type: actionTypes.CLEAR })
+}
+
+/**
+ * Set type
+ * @param type Type
+ */
+export const setType = (type: string) => {
+  selectReducer(initialState, { type: actionTypes.SETTYPE, value: type })
+}
+
+/**
+ * Set part
+ * @param part Part
+ */
+export const setPart = (part: string) => {
+  selectReducer(initialState, { type: actionTypes.SETPART, value: part })
+}
+
+/**
+ * Highlight
+ * @param value { uuid, label }
+ */
+export const highlight = ({ uuid, label }: ISelect) => {
+  selectReducer(initialState, {
+    type: actionTypes.HIGHLIGHT,
+    value: { uuid, label: label }
+  })
+}
+
+/**
+ * Unhighlight
+ */
+export const unhighlight = () => {
+  selectReducer(initialState, { type: actionTypes.UNHIGHLIGHT })
+}
+
+/**
+ * Select
+ * @param value { uuid, label }
+ */
+export const select = ({ uuid, label }: ISelect) => {
+  selectReducer(initialState, {
+    type: actionTypes.SELECT,
+    value: { uuid, label: label }
+  })
+}
+
+/**
+ * Unselect
+ * @param value { uuid, label }
+ */
+export const unselect = ({ uuid, label }: ISelect) => {
+  selectReducer(initialState, {
+    type: actionTypes.UNSELECT,
+    value: { uuid, label: label }
+  })
+}
+
+/**
+ * Props
+ */
+export interface IProps {
+  children: ReactNode
+}
+
+/**
+ * Select provider
+ * @param props Props
+ * @returns SelectProvider
+ */
+const SelectProvider = ({ children }) => {
+  /**
+   * Render
+   */
+  return (
+    <SelectContext.Provider value={initialState}>
+      {children}
+    </SelectContext.Provider>
+  )
+}
+
+export default SelectProvider

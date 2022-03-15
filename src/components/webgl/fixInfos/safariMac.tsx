a605c00301bb34878e7e74657428eaf9245f9300
Simon Garnotel <simon.garnotel@gmail.com>
2022-03-15 11:54:55 +0100

    lint

--- src/components/webgl/fixInfos/safariMac.tsx
+++ src/components/webgl/fixInfos/safariMac.tsx
@@ -11,25 +11,35 @@ const SafariMac = (): JSX.Element => {
   return (
     <Layout>
       <Layout.Content>
-        <Typography.Title level={3}>1- Check MacOS version</Typography.Title>
+        <Typography.Title level={5} mark>
+          1- Check MacOS version
+        </Typography.Title>
         <Typography>
           If you have MacOS version 10.10 Yosemite or a previous one, WebGL
           needs to be activated manually.
         </Typography>
-        <Typography.Title level={3}>2- Developpers Settings</Typography.Title>
+        <Typography.Title level={5} mark>
+          2- Developpers Settings
+        </Typography.Title>
         <Typography>
           Open your browser, then go to Safari -{'>'} Preferences, click
-          "advanced" tab.
+          <span className="text-light">Advanced</span> tab.
         </Typography>
-        <Typography.Title level={3}>3- Show new menu</Typography.Title>
+        <Typography.Title level={5} mark>
+          3- Show new menu
+        </Typography.Title>
         <Typography>
-          At the bottom of the window, check the "Show Develop" menu in menu bar
+          At the bottom of the window, check the{' '}
+          <span className="text-light">Show Develop</span> menu in menu bar
           checkbox.
         </Typography>
-        <Typography.Title level={3}>4- Enable WebGL</Typography.Title>
+        <Typography.Title level={5} mark>
+          4- Enable WebGL
+        </Typography.Title>
         <Typography>
-          Open Develop Tab, check enable WebGL (whether in "Experimental
-          feature" or in the main menu)
+          Open Develop Tab, check enable WebGL (whether in{' '}
+          <span className="text-light">Experimental feature</span> or in the
+          main menu)
         </Typography>
       </Layout.Content>
     </Layout>

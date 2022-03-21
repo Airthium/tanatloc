552a1480a6ea1fc35311e6a58392b3cd1a9623e4
Simon Garnotel <simon.garnotel@gmail.com>
2022-03-21 16:23:26 +0100

    initialization select

--- src/components/project/simulation/initialization/index.tsx
+++ src/components/project/simulation/initialization/index.tsx
@@ -13,7 +13,8 @@ import { Card, Collapse, Layout, Select, Space, Spin, Typography } from 'antd'
 import { ISimulation } from '@/database/index.d'
 import {
   IModelInitialization,
-  IModelInitializationCoupling
+  IModelInitializationCoupling,
+  IModelInitializationValue
 } from '@/models/index.d'
 
 import Formula from '@/components/assets/formula'
@@ -329,7 +330,7 @@ const Initialization = ({
   const [currentKey, setCurrentKey]: [
     string,
     Dispatch<SetStateAction<string>>
-  ] = useState('None')
+  ] = useState()
   const [couplingSimulation, setCouplingSimulation]: [
     ISimulation,
     Dispatch<SetStateAction<ISimulation>>
@@ -375,142 +376,168 @@ const Initialization = ({
       )
   }, [simulations, couplingSimulation])
 
-  // Build initialization
-  const initializationValue = subScheme.value
-  Object.keys(subScheme).forEach((key) => {
-    if (key === 'index' || key === 'title' || key === 'done' || key === 'value')
-      return
-
-    const initialization = subScheme[key]
-
-    if (key === 'coupling') {
-      const couplingInitialization =
-        initialization as IModelInitializationCoupling
-
-      const simulationsOptions = simulations.map((s) => {
-        let disabled = false
-        if (s.id === simulation.id) disabled = true
-        if (
-          !couplingInitialization.compatibility.find(
-            (c) => c.algorithm === s.scheme.algorithm
-          )
-        )
-          disabled = true
-        return { label: s.name, value: s.id, disabled }
-      })
-
-      const compatibility = couplingInitialization.compatibility.find(
-        (c) => c.algorithm === couplingSimulation?.scheme?.algorithm
+  // Build selector
+  const selectorOptions = Object.keys(subScheme)
+    .map((key) => {
+      if (
+        key === 'index' ||
+        key === 'title' ||
+        key === 'done' ||
+        key === 'value'
       )
+        return
 
-      const filter = compatibility?.filter
+      const initialization = subScheme[key] as
+        | IModelInitialization
+        | IModelInitializationCoupling
 
-      content = {
-        ...content,
-        Coupling: (
-          <Card key={key} title={couplingInitialization?.label}>
-            <Typography.Text>
-              If you use coupling, the selected simulation mesh will be used, at
-              least for the first iteration.
-            </Typography.Text>
-            <Space direction="vertical" className="full-width marginTop-10">
-              <Select
-                className="full-width"
-                options={simulationsOptions}
-                placeholder="Select a simulation"
-                defaultValue={initializationValue?.simulation}
-                onChange={async (value: string) => {
-                  setLoading(true)
-                  try {
-                    await onCouplingChange(simulations, simulation, value, swr)
-                    setSimulation(value)
-                  } catch (err) {
-                  } finally {
-                    setLoading(false)
-                  }
-                }}
-              />
-              {loading && <Spin />}
-              {couplingResults?.length ? (
-                <>
-                  <Typography.Text key={'simulation'}>
-                    {filter?.name}:
-                  </Typography.Text>
-                  <Select
-                    options={couplingResults}
-                    placeholder={'Select a ' + filter.name}
-                    defaultValue={initializationValue?.result}
-                    onChange={async (
-                      value: string,
-                      option: { label: string; value: string; file: string }
-                    ) => onCouplingResultChange(simulation, value, option, swr)}
-                  />
-                </>
-              ) : (
-                <Typography.Text type="danger">
-                  No results, please select an other simulation.
-                </Typography.Text>
-              )}
-            </Space>
-          </Card>
-        )
+      return {
+        label: initialization.label,
+        value: key
       }
-    } else {
-      const valueInitialization = initialization as {
-        label: string
-        children: IModelInitialization[]
-      }
-
-      const components = valueInitialization?.children.map(
-        (child: IModelInitialization, index: number) => {
-          if (child.htmlEntity === 'formula') {
-            return (
-              <Formula
-                key={key + '&' + index}
-                label={child.label}
-                defaultValue={
-                  initializationValue?.values?.[index] ||
-                  ((child?.value as string) ?? (child.default as string))
-                }
-                onValueChange={async (value) => {
-                  try {
-                    await onChange(simulation, index, value, swr)
-                  } catch (err) {}
-                }}
-                unit={child.unit}
-              />
-            )
-          } else if (child.htmlEntity === 'select') {
-            return (
-              <Typography.Text key={key + '&' + index}>
-                {child.label}:<br />
-                <Select
-                  options={child.options}
-                  defaultValue={child?.value ?? child.default}
-                  onChange={async (value: string) => {
-                    try {
-                      await onChange(simulation, index, value, swr)
-                    } catch (err) {}
-                  }}
-                />
-              </Typography.Text>
-            )
-          }
-        }
-      )
-
-      content = {
-        ...content,
-        Velocity: (
-          <Card key={key} title={valueInitialization?.label}>
-            <Space direction="vertical" className="full-width">
-              {components}
-            </Space>
-          </Card>
-        )
-      }
-    }
+    })
+    .filter((s) => s)
+  selectorOptions.unshift({
+    label: 'None',
+    value: 'none'
   })
+
+  // const initializationValue = subScheme.value
+  // console.log(initializationValue)
+  // Object.keys(subScheme).forEach((key) => {
+  //   if (key === 'index' || key === 'title' || key === 'done' || key === 'value')
+  //     return
+
+  //   const initialization = subScheme[key]
+
+  //   if (key === 'coupling') {
+  //     const couplingInitialization =
+  //       initialization as IModelInitializationCoupling
+
+  //     const simulationsOptions = simulations.map((s) => {
+  //       let disabled = false
+  //       if (s.id === simulation.id) disabled = true
+  //       if (
+  //         !couplingInitialization.compatibility.find(
+  //           (c) => c.algorithm === s.scheme.algorithm
+  //         )
+  //       )
+  //         disabled = true
+  //       return { label: s.name, value: s.id, disabled }
+  //     })
+
+  //     const compatibility = couplingInitialization.compatibility.find(
+  //       (c) => c.algorithm === couplingSimulation?.scheme?.algorithm
+  //     )
+
+  //     const filter = compatibility?.filter
+
+  //     content = {
+  //       ...content,
+  //       Coupling: (
+  //         <Card key={key} title={couplingInitialization?.label}>
+  //           <Typography.Text>
+  //             If you use coupling, the selected simulation mesh will be used, at
+  //             least for the first iteration.
+  //           </Typography.Text>
+  //           <Space direction="vertical" className="full-width marginTop-10">
+  //             <Select
+  //               className="full-width"
+  //               options={simulationsOptions}
+  //               placeholder="Select a simulation"
+  //               defaultValue={initializationValue?.simulation}
+  //               onChange={async (value: string) => {
+  //                 setLoading(true)
+  //                 try {
+  //                   await onCouplingChange(simulations, simulation, value, swr)
+  //                   setSimulation(value)
+  //                 } catch (err) {
+  //                 } finally {
+  //                   setLoading(false)
+  //                 }
+  //               }}
+  //             />
+  //             {loading && <Spin />}
+  //             {couplingResults?.length ? (
+  //               <>
+  //                 <Typography.Text key={'simulation'}>
+  //                   {filter?.name}:
+  //                 </Typography.Text>
+  //                 <Select
+  //                   options={couplingResults}
+  //                   placeholder={'Select a ' + filter.name}
+  //                   defaultValue={initializationValue?.result}
+  //                   onChange={async (
+  //                     value: string,
+  //                     option: { label: string; value: string; file: string }
+  //                   ) => onCouplingResultChange(simulation, value, option, swr)}
+  //                 />
+  //               </>
+  //             ) : (
+  //               <Typography.Text type="danger">
+  //                 No results, please select an other simulation.
+  //               </Typography.Text>
+  //             )}
+  //           </Space>
+  //         </Card>
+  //       )
+  //     }
+  //   } else {
+  //     const valueInitialization = initialization as {
+  //       label: string
+  //       children: IModelInitialization[]
+  //     }
+
+  //     const components = valueInitialization?.children.map(
+  //       (child: IModelInitialization, index: number) => {
+  //         if (child.htmlEntity === 'formula') {
+  //           return (
+  //             <Formula
+  //               key={key + '&' + index}
+  //               label={child.label}
+  //               defaultValue={
+  //                 initializationValue?.values?.[index] ||
+  //                 ((child?.value as string) ?? (child.default as string))
+  //               }
+  //               onValueChange={async (value) => {
+  //                 try {
+  //                   await onChange(simulation, index, value, swr)
+  //                 } catch (err) {}
+  //               }}
+  //               unit={child.unit}
+  //             />
+  //           )
+  //         } else if (child.htmlEntity === 'select') {
+  //           return (
+  //             <Typography.Text key={key + '&' + index}>
+  //               {child.label}:<br />
+  //               <Select
+  //                 options={child.options}
+  //                 defaultValue={child?.value ?? child.default}
+  //                 onChange={async (value: string) => {
+  //                   try {
+  //                     await onChange(simulation, index, value, swr)
+  //                   } catch (err) {}
+  //                 }}
+  //               />
+  //             </Typography.Text>
+  //           )
+  //         }
+  //       }
+  //     )
+
+  //     content = {
+  //       ...content,
+  //       Velocity: (
+  //         <Card key={key} title={valueInitialization?.label}>
+  //           <Space direction="vertical" className="full-width">
+  //             {components}
+  //           </Space>
+  //         </Card>
+  //       )
+  //     }
+  //   }
+  // })
 
   /**
    * Render
@@ -518,25 +545,24 @@ const Initialization = ({
   return (
     <Layout>
       <Layout.Content>
-        <Select
-          style={{ width: '100%' }}
-          value={currentKey}
-          onChange={async (key) => {
-            try {
-              await onPanelChange(simulation, key, swr)
-              setCurrentKey(key)
-            } catch (err) {}
-          }}
-        >
-          {['Velocity', 'Coupling', 'None'].map((algo) => (
-            <Select.Option key={algo} value={algo}>
-              {algo}
-            </Select.Option>
-          ))}
-        </Select>
-        {Object.entries(content).map(([key, value]) => {
-          return key === currentKey && value
-        })}
+        <Card size="small">
+          <Select
+            style={{ width: '100%' }}
+            defaultValue="none"
+            value={currentKey}
+            options={selectorOptions}
+            onChange={async (key) => {
+              try {
+                await onPanelChange(simulation, key, swr)
+                setCurrentKey(key)
+              } catch (err) {}
+            }}
+          />
+
+          {Object.entries(content).map(([key, value]) => {
+            return key === currentKey && value
+          })}
+        </Card>
       </Layout.Content>
     </Layout>
   )
@@ -544,11 +570,11 @@ const Initialization = ({
 
 Initialization.propTypes = {
   simulations: PropTypes.array,
-  simulation: PropTypes.exact({
+  simulation: PropTypes.shape({
     id: PropTypes.string.isRequired,
     scheme: PropTypes.shape({
       configuration: PropTypes.shape({
-        geometry: PropTypes.object.isRequired
+        initialization: PropTypes.object.isRequired
       }).isRequired
     }).isRequired
   }).isRequired,

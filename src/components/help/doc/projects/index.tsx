41bf96253c5bd7918effb3e70dd5138f131b2acf
Simon Garnotel <simon.garnotel@gmail.com>
2022-03-24 10:27:44 +0100

    minor

--- src/components/help/doc/projects/index.tsx
+++ src/components/help/doc/projects/index.tsx
@@ -12,12 +12,12 @@ const Projects = (): JSX.Element => {
    */
   return (
     <Card title="Project">
-      <Typography.Text>
-        Create a simulation and test it.
-      </Typography.Text>
+      <Typography.Text>Create a simulation and test it.</Typography.Text>
       <Collapse>
         <Collapse.Panel key="archive" header="Archive Project">
-          <Typography.Text>You can archive a project. It will download a .zip of it and</Typography.Text>
+          <Typography.Text>
+            You can archive a project. It will download a .zip of it and
+          </Typography.Text>
         </Collapse.Panel>
         <Collapse.Panel key="groups" header="Groups">
           <Typography.Text>Add, modify or remove groups.</Typography.Text>

ecde3d885a29871616a0329927c8935f1d3a1655
Simon Garnotel <simon.garnotel@gmail.com>
2022-04-21 14:26:51 +0200

    types simulation

--- src/database/simulation/index.ts
+++ src/database/simulation/index.ts
@@ -1,11 +1,23 @@
 /** @module Database.Simulation */
 
 import { INewSimulation, add } from './add'
-import { TSimulationGet, ISimulation, get } from './get'
+import {
+  TSimulationGet,
+  ISimulationTaskFile,
+  ISimulationTask,
+  ISimulation,
+  get
+} from './get'
 import { update } from './update'
 import { del } from './del'
 import { getAll } from './getAll'
 
 const Simulation = { add, get, update, del, getAll }
-export type { INewSimulation, ISimulation, TSimulationGet }
+export type {
+  INewSimulation,
+  ISimulationTaskFile,
+  ISimulationTask,
+  ISimulation,
+  TSimulationGet
+}
 export default Simulation

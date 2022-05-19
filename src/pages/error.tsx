61a78ebb4522bc7fd794cfbf2f395bc6cae41228
WilliamH <58976208+Willix-IT@users.noreply.github.com>
2022-05-19 16:45:03 +0200

    Error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,18 +1,27 @@
 /** @module Pages.Error */
 
 import Error from '@/components/error'
+import React from 'react'
 
 /**
  * Error page
  */
 const error = (): JSX.Element => {
+  const [statusCode, setStatusCode] = React.useState<number>(404)
 
-  const urlParams: URLSearchParams = new URLSearchParams(window.location.search)
-  const statusCode: number = parseInt(urlParams.get('statusCode') || '404', 10)
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const urlParams: URLSearchParams = new URLSearchParams(
+        (window as any).location.search
+      )
+      setStatusCode(parseInt(urlParams.get('statusCode') || '404', 10))
+    }
+  }, [])
+
   /**
    * Render
    */
-  return <Error statusCode={statusCode}/>
+  return <Error statusCode={statusCode} />
 }
 
 export default error

aa5e0c25ff17e766c56fd80f4f10b3b421b40577
WilliamH <58976208+Willix-IT@users.noreply.github.com>
2022-06-13 12:37:52 +0200

    rework indexpage side

--- src/components/assets/side/index.tsx
+++ src/components/assets/side/index.tsx
@@ -3,18 +3,20 @@ export interface IProps {
   right: JSX.Element
   leftClassName?: string
   rightClassName?: string
+  mainClassName?: string
 }
 
 const Side = ({
   left,
   right,
   leftClassName,
-  rightClassName
+  rightClassName,
+  mainClassName
 }: IProps): JSX.Element => {
   return (
-    <div style={{ display: 'flex' }}>
-      <div className={leftClassName}>{left}</div>
-      <div className={rightClassName}>{right}</div>
+    <div className={mainClassName + ' default-side'}>
+      <div className={leftClassName + ' width-50'}>{left}</div>
+      <div className={rightClassName + ' width-50'}>{right}</div>
     </div>
   )
 }

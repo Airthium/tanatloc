95e343a9ca134164fa9cab8459af54060809d27d
Simon Garnotel <simon.garnotel@gmail.com>
2022-11-22 11:59:27 +0100

    jsx css

--- src/components/indexpage/index.style.ts
+++ src/components/indexpage/index.style.ts
@@ -1,37 +1,38 @@
 import { css } from '@emotion/react'
 
 const style = {
-  index: css`
-    display: block;
-    width: 100%;
-    height: 100%;
-    overflow: auto;
-    overflow-x: hidden;
-    padding: 0 100px;
-  `,
-  indexHeader: css`
-    position: sticky;
-    top: 0;
-    z-index: 10;
-    margin: 0 -100px;
-    padding: 20px 120px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background: white !important;
-    margin-bottom: 90px;
-    box-shadow: 0px 0px 28px 8px rgba(0, 0, 0, 0.05);
+  index: css({
+    display: 'block',
+    width: '100%',
+    height: '100%',
+    overflow: 'auto',
+    overflowX: 'hidden',
+    padding: '0 100px'
+  }),
+
+  indexHeader: css({
+    position: 'sticky',
+    top: '0',
+    zIndex: 10,
+    margin: '0 -100px',
+    padding: '20px 120px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    background: 'white !important',
+    marginBottom: '90px',
+    boxShadow: '0px 0px 28px 8px rgba(0, 0, 0, 0.05)',
 
     ul: {
-      flex: 1 1 auto;
-      justify-content: center;
-      border-bottom: none;
+      flex: '1 1 auto',
+      justifyContent: 'center',
+      borderBottom: 'none',
 
       li: {
-        padding: 0 !important;
+        padding: '0 !important'
       }
     }
-  `
+  })
 }
 
 export default style

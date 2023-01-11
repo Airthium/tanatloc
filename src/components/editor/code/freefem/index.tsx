366a04b0dd619f3fa2c7cdf7d62d9bcfb8b55896
WilliamH <58976208+Willix-IT@users.noreply.github.com>
2023-01-11 16:14:43 +0100

    WIP tooltip almost ok

--- src/components/editor/code/freefem/index.tsx
+++ src/components/editor/code/freefem/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useRef } from 'react'
+import { useCallback, useContext, useEffect, useRef, useState } from 'react'
 import AceEditor from 'react-ace'
 import 'ace-builds/src-noconflict/theme-sqlserver'
 import './mode/mode-freefem-ejs'
@@ -7,36 +7,18 @@ import { EditorContext } from '@/context/editor'
 import { setCursor, setTemplate } from '@/context/editor/actions'
 
 /**
- * Set annotations
- * @param selectedText Selected text
- * @returns Annotations
- */
-const setAnnotations = (selectedText: string): any[] => {
-  console.log(selectedText)
-  // Si le texte sélectionné correspond à une fonction connue, retourne une annotation avec un lien vers sa documentation
-  if (selectedText === 'readmesh3') {
-    console.log('dedans')
-    return [
-      {
-        row: 12,
-        column: 0,
-        text: 'Ceci est une fonction. Cliquez ici pour accéder à sa documentation : <a href="https://docs.freefem.org/maFonction">https://docs.freefem.org/maFonction</a>',
-        type: 'info'
-      }
-    ]
-  }
-
-  // Si le texte sélectionné ne correspond pas à une fonction connue, retourne un tableau vide
-  return []
-}
-
-/**
  * FreeFEM code
  */
 const FreeFEMCode = (): JSX.Element => {
   // Data
   const { template, dispatch } = useContext(EditorContext)
   const editorRef = useRef(null)
+  const [tooltipPosition, setTooltipPosition] = useState({
+    x: 0,
+    y: 0,
+    display: false,
+    text: ''
+  })
 
   /**
    * On change
@@ -50,22 +32,44 @@ const FreeFEMCode = (): JSX.Element => {
   )
 
   /**
+   * On Mouse Move
+   * @param event Mouse Event
+   */
+
+  const onMouseMove = (event: MouseEvent) => {
+    if (editorRef.current) {
+      const editor = editorRef.current as AceEditor
+      const textCoords = editor.editor.renderer.pixelToScreenCoordinates(
+        event.clientX,
+        event.clientY
+      )
+      const token = editor.editor.session.getTokenAt(
+        textCoords.row,
+        textCoords.column
+      )
+      if (token && token.type === 'support.function') {
+        setTooltipPosition({
+          x: event.clientX,
+          y: event.clientY,
+          display: true,
+          text: `Documentation for ${token.value}`
+        })
+      } else {
+        setTooltipPosition((prevState) => ({ ...prevState, display: false }))
+      }
+    }
+  }
+
+  if (editorRef.current) {
+    const editor = editorRef.current as AceEditor
+    editor.editor.on('mousemove', onMouseMove)
+  }
+  /**
     
     On cursor change
     */
-  const onSelectionChange = useCallback(
+  const onCursorChange = useCallback(
     (selection: any): void => {
-      if (editorRef.current) {
-        const editor = editorRef.current as AceEditor
-        console.log(editor.editor.session)
-        // Récupère le texte sélectionné
-        const selectedText = editor.editor.session.getTextRange(
-          selection.getRange()
-        )
-        // Définit les annotations à afficher
-        editor.editor.session.setAnnotations(setAnnotations(selectedText))
-      }
-
       dispatch(
         setCursor({
           row: selection.cursor.row,
@@ -81,21 +85,36 @@ const FreeFEMCode = (): JSX.Element => {
     Render
     */
   return (
-    <AceEditor
-      width="100%"
-      height="calc(100% - 64px)"
-      fontSize={16}
-      theme="sqlserver"
-      mode="freefem-ejs"
-      name="freefem_editor"
-      value={template}
-      editorProps={{ $blockScrolling: true, $showPrintMargin: false }}
-      onSelectionChange={onSelectionChange}
-      onChange={onChange}
-      // Affiche les annotations définies
-      annotations={setAnnotations('')}
-      ref={editorRef}
-    />
+    <>
+      <AceEditor
+        width="100%"
+        height="calc(100% - 64px)"
+        fontSize={16}
+        theme="sqlserver"
+        mode="freefem-ejs"
+        name="freefem_editor"
+        value={template}
+        editorProps={{ $blockScrolling: true, $showPrintMargin: false }}
+        onCursorChange={onCursorChange}
+        onChange={onChange}
+        ref={editorRef}
+      />
+
+      {tooltipPosition.display && (
+        <div
+          style={{
+            position: 'absolute',
+            left: tooltipPosition.x,
+            top: tooltipPosition.y,
+            backgroundColor: 'white',
+            padding: '5px',
+            border: '1px solid gray'
+          }}
+        >
+          {tooltipPosition.text}
+        </div>
+      )}
+    </>
   )
 }
 export default FreeFEMCode

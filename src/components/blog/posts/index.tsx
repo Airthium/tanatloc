6268c7c5591e12e91dc687d027047ee9d28e9f98
Houssam Houssein <houssamhoussein6@gmail.com>
2023-02-16 17:57:29 +0100

    Blog add linear elasticity validation test

--- src/components/blog/posts/index.tsx
+++ src/components/blog/posts/index.tsx
@@ -2,7 +2,8 @@
 
 import * as Poisson from './1-poisson'
 import * as Stokes from './2-stokes'
+import * as LinearElasticity from './3-linearElasticity'
 
-const Posts = [Poisson, Stokes]
+const Posts = [Poisson, Stokes, LinearElasticity]
 
 export default Posts

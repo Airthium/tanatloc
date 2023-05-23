a69941d6d3f8045a5768678dff07f5c77d063792
sgarnotel <simon.garnotel@gmail.com>
2023-05-23 09:30:42 +0000

    Deploying to gh-pages from @ Airthium/tanatloc@e907d1b1a2fbc67fe49570ddb52f7d0194a94a30 🚀

---
+++ _next/static/chunks/pages/error-66b5dddfaf84e8be.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2510],{67290:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/error",function(){return _(38818)}])},38818:function(n,u,_){"use strict";_.r(u);var t=_(85893),e=_(34377);let r=()=>(0,t.jsx)(e.Z,{});u.default=r}},function(n){n.O(0,[4377,9774,2888,179],function(){return n(n.s=67290)}),_N_E=n.O()}]);

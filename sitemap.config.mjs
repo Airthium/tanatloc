acca3df0833b083daaa8b3a9ba3fbd795e1603cd
Simon Garnotel <simon.garnotel@gmail.com>
2023-05-24 09:18:11 +0200

    ass sitemap

---
+++ sitemap.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const sitemapConfig = {
+  siteUrl: 'https://tanatloc.com',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }]
+  }
+}
+
+export default sitemapConfig

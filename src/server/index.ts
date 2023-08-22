ccbc5ceed3e9838ee6461a9a5412babad1cb4c95
Simon Garnotel <simon.garnotel@gmail.com>
2023-08-22 17:13:20 +0200

    downgrade next bugfix

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,35 +8,47 @@ import init from './init'
 import clean from './clean'
 
 const port = parseInt(process.env.PORT ?? '3000', 10)
+const hostname = 'localhost'
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 // Initialize
-Object.defineProperty(global, 'tanatloc', { value: {}, configurable: true })
-init().catch((err) => {
-  console.error('Initialize failed!')
-  console.error(err)
-
-  process.exit(1)
+Object.defineProperty(global, 'tanatloc', {
+  value: {},
+  configurable: true
 })
-
-// Server
-app
-  .prepare()
+init()
   .then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url!, true)
-      handle(req, res, parsedUrl).catch(console.error)
-    }).listen(port)
+    // Server
+    app
+      .prepare()
+      .then(() => {
+        createServer((req, res) => {
+          try {
+            const parsedUrl = parse(req.url!, true)
+            handle(req, res, parsedUrl).catch(console.error)
+          } catch (err) {
+            console.error(err)
+            res.statusCode = 500
+            res.end('Internal server error')
+          }
+        }).listen(port)
 
-    console.info(
-      `> Server listening at http://localhost:${port} as ${
-        dev ? 'development' : process.env.NODE_ENV
-      }`
-    )
+        console.info(
+          `> Server listening at http://localhost:${port} as ${
+            dev ? 'development' : process.env.NODE_ENV
+          }`
+        )
+      })
+      .catch(console.error)
   })
-  .catch(console.error)
+  .catch((err) => {
+    console.error('Initialize failed!')
+    console.error(err)
+
+    process.exit(1)
+  })
 
 // Clean
 const handleExit = (code: number): void => {
